11259a7c10544faba4fc57fcc78c656ec8e67e51
Ustinenko Ilya <ilya46ustinenko@gmail.com>
2021-02-22 16:44:41 +0300

    add constructors

--- src/by/gsu/pms/Bus.java
+++ src/by/gsu/pms/Bus.java
@@ -5,7 +5,26 @@ public class Bus {
     private String fullName;
     private int routeNumber;
     private String brand;
-    private int startOfoperation;
+    private int yearOfuse;
     private int mileage;
+
+    public Bus(int busNumber, String fullName, int routeNumber, String brand, int yearOfuse, int mileage) {
+        this.busNumber = busNumber;
+        this.fullName = fullName;
+        this.routeNumber = routeNumber;
+        this.brand = brand;
+        this.yearOfuse = yearOfuse;
+        this.mileage = mileage;
+    }
+
+    public Bus(int busNumber, String fullName, int routeNumber, String brand) {
+        this.busNumber = busNumber;
+        this.fullName = fullName;
+        this.routeNumber = routeNumber;
+        this.brand = brand;
+    }
+
+    public Bus() {
+    }
 
 }
