97c515f7d7f8542da222c9e6477f92f60d37a763
Ustinenko Ilya <ilya46ustinenko@gmail.com>
2021-02-22 12:08:46 +0300

    add fields in class

--- src/by/gsu/pms/Bus.java
+++ src/by/gsu/pms/Bus.java
@@ -1,6 +1,11 @@
 package by.gsu.pms;
 
 public class Bus {
-
+    private int busNumber;
+    private String fullName;
+    private int routeNumber;
+    private String brand;
+    private int startOfoperation;
+    private int mileage;
 
 }
